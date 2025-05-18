199584d485c60bfd2698cfdb25e2e89eea93f60c
kharaneAnand <anandkharane@gmail.com>
2025-05-18 10:29:19 +0530

    created and complted the Doctor.jsx page and update the code

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,11 +1,77 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext';
 
 const Doctors = () => {
+
+  const { speciality } = useParams() 
+  const navigate = useNavigate();
+  const [filterDoc , setFilterDoc] = useState([])
+  const { doctors } = useContext(AppContext)
+
+  const applyFilter = () => {
+    if (speciality) {
+      const decoded = decodeURIComponent(speciality).toLowerCase().trim();
+      setFilterDoc(doctors.filter(doc => doc.speciality.toLowerCase().trim() === decoded));
+    } else {
+      setFilterDoc(doctors);
+    }
+  }
+
+  useEffect(() => {
+    applyFilter()
+  }, [doctors, speciality])
+
   return (
     <div>
-        
+        <p className='text-gray-600'>Browse Through the doctors speciality</p>
+        <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
+            <div className='flex flex-col gap-4 text-sm text-gray-600'>
+                    <p onClick={() => speciality === 'General Physician' ? navigate('/doctors') : navigate('/doctors/General Physician')}
+                      className={`w-[94vw] sm:w-auto whitespace-nowrap pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "General Physician" ? "bg-indigo-100 text-black" : ""}`}>
+                      General Physician
+                    </p>
+
+                    <p onClick={() => speciality === 'Gynecologist' ? navigate('/doctors') : navigate('/doctors/Gynecologist')}
+                      className={`w-[94vw] sm:w-auto whitespace-nowrap pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Gynecologist" ? "bg-indigo-100 text-black" : ""}`}>
+                      Gynecologist
+                    </p>
+
+                    <p onClick={() => speciality === 'Dermatologist' ? navigate('/doctors') : navigate('/doctors/Dermatologist')}
+                      className={`w-[94vw] sm:w-auto whitespace-nowrap pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Dermatologist" ? "bg-indigo-100 text-black" : ""}`}>
+                      Dermatologist
+                    </p>
+
+                    <p onClick={() => speciality === 'Pediatrician' ? navigate('/doctors') : navigate('/doctors/Pediatrician')}
+                      className={`w-[94vw] sm:w-auto whitespace-nowrap pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Pediatrician" ? "bg-indigo-100 text-black" : ""}`}>
+                      Pediatrician
+                    </p>
+
+                    <p onClick={() => speciality === 'Neurologist' ? navigate('/doctors') : navigate('/doctors/Neurologist')}
+                      className={`w-[94vw] sm:w-auto whitespace-nowrap pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Neurologist" ? "bg-indigo-100 text-black" : ""}`}>
+                      Neurologist
+                    </p>
+              </div>
+
+            <div className='w-full  grid grid-cols-auto gap-4 gap-y-6'>
+              {
+                filterDoc.map((item , index)=>(
+                  <div onClick={()=>navigate(`/appointment/${item._id}`)} className=' border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-15px] transition-all duration-500'  key ={index}>
+                    <img className=' w-full h-52 object-cover object-top bg-blue-50' src={item.image} alt="doctors image" />
+                        <div className='p-4'>
+                        <div className='flex items-center gap-2 text-sm text-center  text-green-500'>
+                            <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Available</p>
+                        </div>
+                        <p className='text-grey-900 text-lg font-medium'>{item.name}</p>
+                        <p className='text-grey-600 text-sm'>{item.speciality}</p>
+                    </div>
+                  </div>
+                ))
+              }
+            </div>
+        </div>
     </div>
   )
 }
 
-export default Doctors
+export default Doctors;
